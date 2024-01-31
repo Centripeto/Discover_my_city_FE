84f44ab0d0277e67a181db03978cb8e210bded88
Mattia Izzi <izzimattia@yahoo.it>
2024-01-31 14:17:31 +0100

    apertura dialog login visualizzazione desktop

--- src/layout/layout.component.js
+++ src/layout/layout.component.js
@@ -20,8 +20,24 @@ import HomeIcon from "@mui/icons-material/Home";
 import PlaceIcon from "@mui/icons-material/Place";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
-import { ListItemButton } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  FormControl,
+  Grid,
+  InputAdornment,
+  ListItemButton,
+  TextField,
+  useMediaQuery,
+} from "@mui/material";
 import { useThemeMode } from "../providers/ThemeModeProvider";
+import { useAuth } from "../providers/AuthProvider";
+import { useForm } from "react-hook-form";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import LoginIcon from '@mui/icons-material/Login';
 
 const drawerWidth = 240;
 
@@ -127,6 +143,119 @@ const DrawerMobile = ({
   );
 };
 
+const LoginDialog = ({ open, handleClose, login }) => {
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
+  const [showPassword, setShowPassword] = useState(false);
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, submitCount, isValid },
+  } = useForm({
+    defaultValues: {},
+  });
+
+  const handleClickShowPassword = () => setShowPassword((prev) => !prev);
+
+  const username = register("username", {
+    required: "Required",
+  });
+
+  const password = register("password", {
+    required: "Required",
+  });
+
+  const onSubmit = (form) => {
+    try {
+      login({ username: form.username, password: form.password });
+      handleClose();
+    } catch (ex) {
+      // TODO handle message
+      console.log(ex);
+    }
+  };
+  return (
+    <Dialog
+      fullScreen={fullScreen}
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="login-dialog"
+    >
+      <DialogTitle id="login-dialog">Login</DialogTitle>
+      <DialogContent>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Grid container spacing={1}>
+            <Grid container item xs={12} spacing={3}>
+              <Grid item xs={4}>
+                <FormControl fullWidth variant="outlined">
+                  <TextField
+                    id="username"
+                    helperText={
+                      errors.username ? errors.username.message : null
+                    }
+                    variant="outlined"
+                    label="Username"
+                    error={!!errors.username}
+                    name={username.name}
+                    onBlur={username.onBlur}
+                    onChange={username.onChange}
+                    inputRef={username.ref}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid item xs={4}>
+                <FormControl fullWidth variant="outlined">
+                  <TextField
+                    id="password"
+                    type={showPassword ? "text" : "password"}
+                    helperText={
+                      errors.password ? errors.password.password : null
+                    }
+                    variant="outlined"
+                    label="Password"
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            edge="end"
+                          >
+                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                          </IconButton>
+                        </InputAdornment>
+                      ),
+                    }}
+                    error={!!errors.password}
+                    name={password.name}
+                    onBlur={password.onBlur}
+                    onChange={password.onChange}
+                    inputRef={password.ref}
+                  />
+                </FormControl>
+              </Grid>
+            </Grid>
+            <Grid item></Grid>
+          </Grid>
+        </form>
+      </DialogContent>
+      <DialogActions>
+        <Button autoFocus onClick={handleClose}>
+          Cancel
+        </Button>
+        <Button
+          type="submit"
+          disabled={submitCount > 0 && !isValid}
+          variant="outlined"
+          color="primary"
+        >
+          Login
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
 const sideMenu = [
   {
     label: "Home",
@@ -143,7 +272,26 @@ const sideMenu = [
 const Layout = () => {
   const theme = useTheme();
   const { toggleColorMode } = useThemeMode();
+  const { login, accessToken } = useAuth();
+  const [isLoginDialogOpen, setLoginDialogOpen] = useState(false);
   const [open, setOpen] = useState(false);
+
+  const ChangeThemeIcon = () => (
+    <IconButton sx={{ ml: 1 }} onClick={toggleColorMode} color="inherit">
+          {theme.palette.mode === "dark" ? (
+            <Brightness7Icon />
+          ) : (
+            <Brightness4Icon />
+          )}
+        </IconButton>
+  );
+
+  const handleLoginDialogClose = () => {
+    setLoginDialogOpen(false);
+  };
+  const handleLoginDialogOpen = () => {
+    setLoginDialogOpen(true);
+  };
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -156,14 +304,41 @@ const Layout = () => {
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar position="fixed" open={open}>
+      <AppBar
+        position="fixed"
+        open
+        sx={{ display: { xs: "none", sm: "block" } }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div" flexGrow={1}>
+            Discover My City
+          </Typography>
+          {!accessToken ? (
+            <IconButton
+              color="inherit"
+              aria-label="open login"
+              onClick={handleLoginDialogOpen}
+              edge="start"
+              xs={{ mr: 2 }}
+            >
+              <LoginIcon />
+            </IconButton>
+          ) : null}
+          <ChangeThemeIcon />
+        </Toolbar>
+      </AppBar>
+      <AppBar
+        position="fixed"
+        open={open}
+        sx={{ display: { xs: "block", sm: "none" } }}
+      >
         <Toolbar>
           <IconButton
             color="inherit"
             aria-label="open drawer"
             onClick={handleDrawerOpen}
             edge="start"
-            sx={{ mr: 2, display: { sm: "none" } }}
+            xs={{ mr: 2 }}
           >
             <MenuIcon />
           </IconButton>
@@ -172,6 +347,7 @@ const Layout = () => {
           </Typography>
         </Toolbar>
       </AppBar>
+
       <Box
         component="nav"
         sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
@@ -194,14 +370,14 @@ const Layout = () => {
         }}
       >
         <DrawerHeader />
-        <IconButton sx={{ ml: 1 }} onClick={toggleColorMode} color="inherit">
-          {theme.palette.mode === "dark" ? (
-            <Brightness7Icon />
-          ) : (
-            <Brightness4Icon />
-          )}
-        </IconButton>
         <Outlet />
+        {!accessToken ? (
+          <LoginDialog
+            open={isLoginDialogOpen}
+            handleClose={handleLoginDialogClose}
+            login={login}
+          />
+        ) : null}
       </Box>
     </Box>
   );
